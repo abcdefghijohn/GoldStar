8318279ff5fa88e07a4e19f62f6c85a4090af50d
John Kim <abcdefghijohnk@gmail.com>
2021-02-06 15:24:51 -0700

    Initial Commit

---
+++ gs/gsApp.swift
@@ -0,0 +1,17 @@
+//
+//  gsApp.swift
+//  gs
+//
+//  Created by John Kim on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct gsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
